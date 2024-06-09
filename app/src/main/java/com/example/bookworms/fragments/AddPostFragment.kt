336d1cb8abd6735285f0a90ab7dbbf952d3a7643
Tomer <tomermi123@gmail.com>
2024-06-09 23:58:27 +0300

    feat: basic add post fragment layout

---
+++ app/src/main/java/com/example/bookworms/fragments/AddPostFragment.kt
@@ -0,0 +1,23 @@
+package com.example.bookworms.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.bookworms.R
+
+/**
+ * A simple `Fragment` subclass.
+ * Use the `AddPostFragment.newInstance` factory method to create an instance of this fragment.
+ */
+class AddPostFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                            savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_add_post, container, false)
+    }
+}
