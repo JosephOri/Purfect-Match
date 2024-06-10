7f07ac883e7607cb7a35eaf5e99d3c8f48f4d09e
Tomer <tomermi123@gmail.com>
2024-06-10 13:35:49 +0300

    feat: adding fragments for navbar menu.

---
+++ app/src/main/java/com/example/bookworms/fragments/ProfilePageFragment.kt
@@ -0,0 +1,28 @@
+package com.example.bookworms.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.bookworms.R
+
+
+/**
+ * A simple `Fragment` subclass.
+ * Use the `ProfilePageFragment.newInstance` factory method to
+ * create an instance of this fragment.
+ */
+class ProfilePageFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        return inflater.inflate(R.layout.fragment_profile_page, container, false)
+    }
+
+}
