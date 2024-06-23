2519015a6259b092f5065d5cd35ef20be1a69060
Tomer <tomermi123@gmail.com>
2024-06-23 22:45:53 +0300

    Feat: Added getUserByUid function to UserFirebaseModel.

--- app/src/main/java/com/example/bookworms/models/firebaseModel/UserFirebaseModel.kt
+++ app/src/main/java/com/example/bookworms/models/firebaseModel/UserFirebaseModel.kt
@@ -1,5 +1,6 @@
 package com.example.bookworms.models.firebaseModel
 
+import com.example.bookworms.models.entities.User
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
@@ -26,6 +27,7 @@ class UserFirebaseModel {
             }
 
     }
+
     fun userCollection(
         email: String,
         uid: String,
@@ -49,5 +51,40 @@ class UserFirebaseModel {
             callback(false)
         }
     }
+
+    fun getUserByUid(uid: String, callback: (User?) -> Unit) {
+        val db = Firebase.firestore
+        val usersCollection = db.collection("users")
+        println("Getting user by UID: $uid")
+
+        usersCollection.whereEqualTo("uid", uid)
+            .get()
+            .addOnSuccessListener { querySnapshot ->
+                if (!querySnapshot.isEmpty) {
+                    println("Document found with UID $uid. querySnapshot is not empty.")
+                    val documentSnapshot = querySnapshot.documents.first()
+                    val userName = documentSnapshot.getString("name") ?: ""
+                    val email = documentSnapshot.getString("email") ?: ""
+                    val phone = documentSnapshot.getString("phone") ?: ""
+
+                    val userEntity = User(
+                        uid = uid,
+                        name = userName,
+                        email = email,
+                        phone = phone
+                    )
+
+                    callback(userEntity)
+                } else {
+                    println("No document found with UID $uid. querySnapshot is empty.")
+                    callback(null)
+                }
+            }
+            .addOnFailureListener { exception ->
+                println("Error getting user by UID: ${exception.message}")
+                callback(null)
+            }
+    }
+
 
 }
