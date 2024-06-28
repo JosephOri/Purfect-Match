b68caf148c95d246b582215976cdf6888cef9a44
Tomer <tomermi123@gmail.com>
2024-06-28 19:31:26 +0300

    Refactor: add profile image to page, finishing merge

--- app/src/main/java/com/example/bookworms/fragments/MapsFragment.kt
+++ app/src/main/java/com/example/bookworms/fragments/MapsFragment.kt
@@ -9,7 +9,6 @@ import android.view.ViewGroup
 import com.example.bookworms.R
 
 import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
@@ -27,9 +26,9 @@ class MapsFragment : Fragment() {
          * install it inside the SupportMapFragment. This method will only be triggered once the
          * user has installed Google Play services and returned to the app.
          */
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val telAviv = LatLng(32.0853, 34.7818)
+        googleMap.addMarker(MarkerOptions().position(telAviv).title("Marker in Tel Aviv"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(telAviv))
     }
 
     override fun onCreateView(
